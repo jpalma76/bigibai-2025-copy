28bffd0b053fe75cd2117732293680be2be5df35
Juan Palma <juan.palma.alvarez@gmail.com>
2025-10-16 13:37:17 -0300

    add  subscribe

---
+++ src/newsletter/services/subscribe.ts
@@ -0,0 +1,18 @@
+import { supabase } from "@/supabase";
+
+export const saveNewsLetterEmail = async (email: string) => {
+  const { data, error } = await supabase.from('newsletter').insert({email})
+  if(error) {
+    console.log(error);
+    
+    return {
+      success: false,
+      error: "Error al guardar el email en la newsletter"
+    }
+  }
+
+  return {
+    success:true,
+    error: null
+  }
+}
