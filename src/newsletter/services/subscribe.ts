dce512beb5110736c82ed26dcc362e9bd22fd254
Juan Palma <juan.palma.alvarez@gmail.com>
2025-10-16 23:01:56 -0300

    add duplicated

--- src/newsletter/services/subscribe.ts
+++ src/newsletter/services/subscribe.ts
@@ -1,18 +1,30 @@
 import { supabase } from "@/supabase";
 
+const ERROR_CODE_ALREADY_EXIST ="23505"
+
 export const saveNewsLetterEmail = async (email: string) => {
-  const { data, error } = await supabase.from('newsletter').insert({email})
+  const { error } = await supabase.from('newsletter').insert({email})
   
+  if (error?.code === ERROR_CODE_ALREADY_EXIST) {
+    return {
+      duplicated: true,
+      success: false,
+      error: "¡El email ya existe en la newsletter!"
+    }
+  }
+
   if(error) {
-    console.log(error);
+    console.error(error);
     
     return {
+      duplicated: false,
       success: true,
       error: "Error al guardar el email en la newsletter"
     }
   }
 
   return {
+    duplicated: false,
     success:true,
     error: null
   }
